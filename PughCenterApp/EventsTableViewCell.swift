d5d01bd7761928089a3c9bf28ce055d7f966e8fb
Iavor Dekov <ivdekov@gmail.com>
2016-02-09 18:57:34 -0500

    Added sliding menu and events view controller

---
+++ PughCenterApp/EventsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventsTableViewCell.swift
+//  PughCenterApp
+//
+//  Created by Iavor Dekov on 2/9/16.
+//  Copyright © 2016 Iavor Dekov. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
