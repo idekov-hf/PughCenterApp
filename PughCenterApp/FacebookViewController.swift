0167375b814dfd8b15f2fc9e8988b4f2112b8c07
Iavor V. Dekov <ivdekov@imac-023269.colby.edu>
2016-02-18 09:43:01 -0500

    Add new view controller with WebView for Pugh Center Facebook page.

---
+++ PughCenterApp/FacebookViewController.swift
@@ -0,0 +1,55 @@
+//
+//  FacebookViewController.swift
+//  PughCenterApp
+//
+//  Created by Iavor V. Dekov on 2/18/16.
+//  Copyright © 2016 Iavor Dekov. All rights reserved.
+//
+
+import UIKit
+
+class FacebookViewController: UIViewController {
+
+    @IBOutlet var menuButton: UIBarButtonItem!
+    @IBOutlet var webView: UIWebView!
+    
+    var screenWidth: CGFloat = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.viewDidLayoutSubviews()
+        
+        if revealViewController() != nil {
+            revealViewController().rearViewRevealWidth = screenWidth / 2
+            menuButton.target = revealViewController()
+            menuButton.action = "revealToggle:"
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+            let url = NSURL (string: "https://www.facebook.com/colby.pugh.10");
+            let requestObj = NSURLRequest(URL: url!);
+            webView.loadRequest(requestObj);
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.screenWidth = self.view.frame.size.width
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
