274d1e27e6269fdfd60cd3dbf656c71bde8f4ec9
Iavor Dekov <ivdekov@gmail.com>
2016-03-27 15:40:15 -0400

    Add push notifications, add custom data to test push notifications

--- PughCenterApp/EventsTableViewController.swift
+++ PughCenterApp/EventsTableViewController.swift
@@ -22,13 +22,14 @@ class EventsTableViewController: UITableViewController {
     var screenWidth: CGFloat = 0
     var events = [Event]()
     var selectedIndexPath: NSIndexPath?
+    var deSelectedIndexPath: NSIndexPath?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Enables self sizing cells
         // http://www.appcoda.com/self-sizing-cells/
-        tableView.estimatedRowHeight = 65.0
+        tableView.estimatedRowHeight = 80
         tableView.rowHeight = UITableViewAutomaticDimension
         
         self.viewDidLayoutSubviews()
@@ -52,11 +53,34 @@ class EventsTableViewController: UITableViewController {
         // Parse and set events field
         // cellForRowAtIndexPath uses the data 
         // from the events data field when the data is reloaded
-        let eventParser = EventParser()
-        eventParser.beginParsing()
-        events = eventParser.events
+//        let eventParser = EventParser()
+//        eventParser.beginParsing()
+//        events = eventParser.events
+        
+        // test local notifications with custom data
+        loadCustomData()
+        
+        addLocalNotifications()
         activityIndicator.stopAnimating()
         tableView.reloadData()
+    }
+    
+    func loadCustomData() {
+        let event1 = Event(title: "Event 1", description: "This is a description of Event 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris scelerisque porttitor nunc, a vestibulum odio faucibus sed. Morbi in tortor nec enim rutrum auctor a eget leo. Integer vel dapibus turpis. Aliquam tempor vehicula sapien id varius. Duis nec sapien id eros imperdiet ullamcorper. ", startDate: NSDate().dateByAddingTimeInterval(1815))
+        let event2 = Event(title: "Event 2", description: "This is a short description of Event 2", startDate: NSDate().dateByAddingTimeInterval(1830))
+        let event3 = Event(title: "Event 3", description: "This is a description of Event 3: Proin enim arcu, commodo eu tellus eu, eleifend iaculis eros. Nam id ante placerat, malesuada metus eu, varius dolor. In eu eros posuere magna tincidunt maximus nec sed dui. Nam condimentum massa ut auctor porta.", startDate: NSDate().dateByAddingTimeInterval(1845))
+        events += [event1, event2, event3]
+    }
+    
+    func addLocalNotifications() {
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        for event in events {
+            let notification = UILocalNotification()
+            notification.fireDate = event.startDate?.dateByAddingTimeInterval(-1800)
+            notification.alertBody = event.title + " is starting in 30 minutes."
+            notification.soundName = UILocalNotificationDefaultSoundName
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        }
     }
     
     override func viewDidLayoutSubviews() {
@@ -65,12 +89,10 @@ class EventsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return events.count
     }
 
@@ -89,6 +111,7 @@ class EventsTableViewController: UITableViewController {
         cell.dateLabel.text = dateAsString
         
         if indexPath == selectedIndexPath {
+            cell.backgroundColor = UIColor.lightGrayColor()
             cell.descriptionLabel.text = events[indexPath.row].eventDescription
         }
         
@@ -96,33 +119,19 @@ class EventsTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        var indexPathArray = [NSIndexPath]()
+        
+        deSelectedIndexPath = selectedIndexPath
+        if let deSelectedIndexPath = deSelectedIndexPath {
+            indexPathArray.append(deSelectedIndexPath)
+        }
+        
         selectedIndexPath = indexPath
-        tableView.reloadData()
+        indexPathArray.append(indexPath)
+        
+        tableView.reloadRowsAtIndexPaths(indexPathArray, withRowAnimation: UITableViewRowAnimation.Automatic)
+        tableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Middle, animated: true)
+        
     }
-    
-//    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-//        selectedIndexPath = indexPath
-//        tableView.beginUpdates()
-//        tableView.endUpdates()
-//        let cell = tableView.cellForRowAtIndexPath(indexPath) as! EventsTableViewCell
-//        cell.descriptionLabel.text = events[indexPath.row].eventDescription
-//        tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
-//        tableView.reloadData()
-//    }
-    
-//    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-//        let cell = tableView.cellForRowAtIndexPath(indexPath) as! EventsTableViewCell
-//        cell.descriptionLabel.text = ""
-//        tableView.reloadData()
-//    }
-
-//    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-//        if (selectedIndexPath == indexPath) {
-//            let cell = tableView.cellForRowAtIndexPath(indexPath) as! EventsTableViewCell
-//            cell.descriptionLabel.hidden = false
-//            return cell.frame.height
-//        }
-//        return UITableViewAutomaticDimension
-//    }
 
 }
