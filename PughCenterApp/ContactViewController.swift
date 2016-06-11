d592904a73b537b3ffe1806760452421f668f420
Iavor Dekov <ivdekov@gmail.com>
2016-06-11 10:28:15 -0400

    Add networking code in ContactViewController

--- PughCenterApp/ContactViewController.swift
+++ PughCenterApp/ContactViewController.swift
@@ -15,6 +15,8 @@ class ContactViewController: UIViewController {
     
     var screenWidth: CGFloat = 0
     
+    let url = NSURL(string: "http://www.colby.edu/pugh/wp-json/colby-rest/v0/acf-options?additional_contacts=1")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,11 +29,71 @@ class ContactViewController: UIViewController {
             
             view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        loadContactInfo()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewWillLayoutSubviews()
         self.screenWidth = self.view.frame.size.width
+    }
+    
+    func loadContactInfo() {
+        
+        // create request
+        let session = NSURLSession.sharedSession()
+        let request = NSURLRequest(URL: url)
+        
+        // create network request
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            // if an error occurs, print it
+            func displayError(error: String) {
+                print(error)
+            }
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                displayError("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                displayError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                displayError("No data was returned by the request!")
+                return
+            }
+            
+            // parse the data
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                displayError("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            guard let text = parsedResult["additional_contacts"] as? [[String : AnyObject]] else {
+                print("Contact info not succesfully extracted")
+                return
+            }
+            
+            print(text)
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                
+            }
+            
+        }
+        
+        // start the task
+        task.resume()
     }
 
 }
